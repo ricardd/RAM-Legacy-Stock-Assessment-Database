4d45af9a83f4f0559d1a7a88bbaee4827b0eb832
Daniel Ricard <ricardd@mathstat.dal.ca>
2010-06-03 00:01:38 -0300

    Another commit before I go to bed.
    Mostly new files for data request from Malin Pinsky. Also, modifications to CJFAS makefile.

---
+++ datarequests/Pinsky-20100602.sql
@@ -0,0 +1,18 @@
+-- data extraction for Malin Pinsky
+--
+-- notes from Skype call on June 02 2010
+--
+--each stock a line
+--each column is a year
+--ratios in there
+
+-- get the stocks that have an MSY reference point
+-- one of SSB or TB, just one, with a reference point, if they both have reference points, chose SSB preferentially
+
+---no SSB but TB and Bmsy
+--1950 to the present
+
+--after the fact, Malin trims out non-species and does manual edits for scientific names that are not in fishbase
+-- WHAT ARE THOSE SPECIES?
+
+-- + comapre the trophic leve data that I have with his + add other life-history characterisics
